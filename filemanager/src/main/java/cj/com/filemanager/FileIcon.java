8a4aaae911336e20d58fa984cb9060870ec0feaf
Chris <Chris_12456@hotmail.com>
2018-01-24 12:21:05 -0500

    Refactor FileIcon to initialize the mapping from file extension to drawable id once per session.
    Add header comment to FileIcon.

--- filemanager/src/main/java/cj/com/filemanager/FileIcon.java
+++ filemanager/src/main/java/cj/com/filemanager/FileIcon.java
@@ -3,24 +3,25 @@ package cj.com.filemanager;
 import java.util.HashMap;
 
 /**
- * Created by EpiphX on 1/14/18.
+ * File icon provides mapping from the given file extension to the appropriate drawable id.
  */
-
 public class FileIcon {
-    private int mDrawableId;
+    private int mDrawableId = 0;
+    private static HashMap<String, Integer> mExtensionTypeToDrawableMap = new HashMap<>();
+
+    static {
+        mExtensionTypeToDrawableMap.put("pdf", R.drawable.ic_pdf);
+        mExtensionTypeToDrawableMap.put("jpeg", R.drawable.ic_jpg);
+        mExtensionTypeToDrawableMap.put("jpg", R.drawable.ic_jpg);
+        mExtensionTypeToDrawableMap.put("png", R.drawable.ic_png);
+        mExtensionTypeToDrawableMap.put("mp3", R.drawable.ic_mp3);
+        mExtensionTypeToDrawableMap.put("svg", R.drawable.ic_svg);
+        mExtensionTypeToDrawableMap.put("txt", R.drawable.ic_txt);
+    }
 
     public FileIcon(String fileExtension) {
-        HashMap<String, Integer> extensionTypeToDrawableMap = new HashMap<>();
-        extensionTypeToDrawableMap.put("pdf", R.drawable.ic_pdf);
-        extensionTypeToDrawableMap.put("jpeg", R.drawable.ic_jpg);
-        extensionTypeToDrawableMap.put("jpg", R.drawable.ic_jpg);
-        extensionTypeToDrawableMap.put("png", R.drawable.ic_png);
-        extensionTypeToDrawableMap.put("mp3", R.drawable.ic_mp3);
-        extensionTypeToDrawableMap.put("svg", R.drawable.ic_svg);
-        extensionTypeToDrawableMap.put("txt", R.drawable.ic_txt);
-
-        if (extensionTypeToDrawableMap.containsKey(fileExtension)) {
-            mDrawableId = extensionTypeToDrawableMap.get(fileExtension);
+        if (mExtensionTypeToDrawableMap.containsKey(fileExtension)) {
+            mDrawableId = mExtensionTypeToDrawableMap.get(fileExtension);
         } else {
             mDrawableId = R.drawable.ic_txt;
         }
